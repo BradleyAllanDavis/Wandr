45bfe8bb9dcaf7bcb118b35d4d77ce398f17727f
Scott Franklin <stfranklin@wisc.edu>
2017-03-09 18:05:29 -0600

    Added the table view to the cartViewController, implemented the necessary protocol methods to set up the actual Table View, used filler data to start

--- TravelApp/CartViewController.swift
+++ TravelApp/CartViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class CartViewController: UIViewController {
+class CartViewController: UIViewController, UITableViewDataSource {
+    let data: [String] = ["Copenhagen", "Amsterdam", "Paris", "Barcelona", "Budapest", "Berlin"] // dummy filler data to test that the table views cells are set
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
