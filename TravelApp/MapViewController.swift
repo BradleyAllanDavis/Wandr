0b4668674222ea6cdde906bf3a8aeb8a94a69fca
Bradley Davis <bad884@me.com>
2017-03-13 22:23:02 -0500

    Add icon pack. Rename tab labels and select icons. Add MapKit view to map view.

--- TravelApp/MapViewController.swift
+++ TravelApp/MapViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
