f9e90bb124b27e076a5a191f404fdd65588aba1f
Scott Franklin <stfranklin@wisc.edu>
2017-04-11 13:45:16 -0500

    Adding components for PlaceDetail Navigation from the Cart Tableview

--- TravelApp/CartViewController.swift
+++ TravelApp/CartViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CartViewController: UIViewController, UITableViewDataSource {
+class CartViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let data: [String] = ["Copenhagen", "Amsterdam", "Paris", "Barcelona", "Budapest", "Berlin"] // dummy filler data to test that the table views cells are set
     
     override func viewDidLoad() {
@@ -24,6 +24,17 @@ class CartViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath)
         cell.textLabel?.text = data[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected")
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let row = indexPath.row
+        print("Row: \(row)")
+        
+        let destination = PlaceDetailViewController() // Your destination
+        navigationController?.pushViewController(destination, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
---
+++ TravelApp/PlaceDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceDetailViewController.swift
+//  TravelApp
+//
+//  Created by Macbook on 4/11/17.
+//  Copyright © 2017 Scott Franklin. All rights reserved.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
